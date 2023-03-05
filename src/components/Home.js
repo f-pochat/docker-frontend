d94e8ac125a1b52da0f1f6c9be20397576c57c80
f-pochat <federico.pochat@ing.austral.edu.ar>
2023-03-05 14:36:56 -0300

    v2

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 const Home = () => {
     return (
         <div className="flex-column d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
-            <h1>Home</h1>
+            <h1>Home v2</h1>
             <Link to="/" className="btn btn-primary">Go to Main</Link>
         </div>
     )
